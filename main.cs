f9718f410f38d3b35190a1506b017aa961a78773
Webhe Rosa da Costa <webhecosta@hotmail.com>
2020-08-18 22:12:23 +0000

    Novos WriteLine

--- main.cs
+++ main.cs
@@ -3,5 +3,7 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Hello World");
   }
 }
